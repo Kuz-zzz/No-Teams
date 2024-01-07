d8789e01a78e7cf4e4f9e5f084bd59e2fefde7ee
Kuz_ <37791516+Kuz-zzz@users.noreply.github.com>
2024-01-07 14:04:44 +0500

    Make thinks a little more configurable

---
+++ NoTeams/Config.cs
@@ -0,0 +1,21 @@
+﻿namespace NoTeams
+{
+    public class Config
+    {
+        public bool enabled;
+        public string message = "";
+        public bool kickOnSwitchAttempt;
+
+        public static Config DefaultConfig()
+        {
+            Config vConf = new Config
+            {
+                enabled = true,
+                message = "You cannot join teams!",
+                kickOnSwitchAttempt = false,
+            };
+
+            return vConf;
+        }
+    }
+}
--- NoTeams/NoTeams.cs
+++ NoTeams/NoTeams.cs
@@ -1,6 +1,8 @@
 ﻿using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
+using Newtonsoft.Json;
+
 
 
 namespace NoTeams
@@ -14,7 +16,11 @@ namespace NoTeams
 
         public override string Name => "No Teams";
 
-        public override Version Version => new Version(1, 0, 0, 0);
+        public override Version Version => new Version(1, 1, 0, 0);
+
+        private static Config config;
+        internal static string filepath { get { return Path.Combine(TShock.SavePath, "noteams.json"); } }
+        public static bool NoTeamsOn = true;
 
         public NoTeams(Main game) : base(game)
         {
@@ -23,7 +29,15 @@ namespace NoTeams
         public override void Initialize()
         {
             GetDataHandlers.PlayerTeam += OnTeamChange;
-
+            ReadConfig(filepath, Config.DefaultConfig(), out config);
+            Commands.ChatCommands.Add(new Command("tshock.noteams", CommandHandler, "noteams"));
+            if (config.enabled)
+            {
+                NoTeamsOn = true;
+            } else
+            {
+                NoTeamsOn = false;
+            }
         }
 
         protected override void Dispose(bool disposing)
@@ -35,14 +49,66 @@ namespace NoTeams
             base.Dispose(disposing);
         }
 
+        private static void ReadConfig<TConfig>(string path, TConfig defaultConfig, out TConfig config)
+        {
+            if (!File.Exists(path))
+            {
+                config = defaultConfig;
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+            }
+            else
+            {
+                config = JsonConvert.DeserializeObject<TConfig>(File.ReadAllText(path));
+
+            }
+        }
+
+        private void CommandHandler(CommandArgs args)
+        {
+            if (args.Parameters.Count != 0)
+            {
+                if (args.Parameters[0] == "on")
+                {
+                    NoTeamsOn = true;
+                    args.Player.SendSuccessMessage("Players can no longer switch teams!");
+                }
+                else if (args.Parameters[0] == "off")
+                {
+                    NoTeamsOn = false;
+                    args.Player.SendSuccessMessage("Players now can switch teams!");
+                } 
+                else if (args.Parameters[0] == "reload")
+                {
+                    ReadConfig(filepath, Config.DefaultConfig(), out config);
+                    args.Player.SendSuccessMessage("Config was successfully reloaded");
+                }
+                else {
+                    args.Player.SendErrorMessage("Invalid syntaxis! Usage: /noteams <on/off>");
+                }
+            }
+            else
+            {
+                args.Player.SendErrorMessage("Invalid syntaxis! Usage: /noteams <on/off>");
+            }
+        }
+
         void OnTeamChange(object sender, GetDataHandlers.PlayerTeamEventArgs args)
         {
-            TSPlayer tSPlayer = args.Player;
+            if (NoTeamsOn)
+            {
+                TSPlayer player = args.Player;
 
-            tSPlayer.SetTeam(0); // sets player's team back to white
-            tSPlayer.SendErrorMessage("You cannot join teams!"); // sends a message that can only be seen by the player
+                if (config.kickOnSwitchAttempt)
+                {
+                    player.Kick(config.message);
+                    return;
+                }
 
-            args.Handled = true; // makes it so "PlayerName joined the TeamName party!" doesn't appear
+                player.SetTeam(0); // sets player's team back to white
+                player.SendErrorMessage(config.message); // sends a message that can only be seen by the player
+
+                args.Handled = true; // makes it so "PlayerName joined the TeamName party!" doesn't appear
+            }            
         }
     }
 }
